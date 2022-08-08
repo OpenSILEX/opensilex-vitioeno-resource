3e8d33b346e58ded57cbb78ce9c8003c9f577d20
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2022-08-08 08:26:04 +0200

    Add missing imports

--- src/main/java/inrae/vitioeno/VitioenoModule.java
+++ src/main/java/inrae/vitioeno/VitioenoModule.java
@@ -5,24 +5,27 @@
 //******************************************************************************
 package inrae.vitioeno;
 
+import java.util.List;
+import org.apache.jena.riot.Lang;
 import org.opensilex.OpenSilexModule;
+import org.opensilex.sparql.extensions.OntologyFileDefinition;
+import org.opensilex.sparql.extensions.SPARQLExtension;
 
 /**
  * BigDataGrapes module implementation
  */
-public class VitioenoModule extends OpenSilexModule {
+public class VitioenoModule extends OpenSilexModule implements SPARQLExtension{
 
     @Override
     public List<OntologyFileDefinition> getOntologiesFiles() throws Exception {
-    List<OntologyFileDefinition> list =
-    SPARQLExtension.super.getOntologiesFiles();
-    list.add(new OntologyFileDefinition(
-    "http://vitioeno.inrae.fr/vocabulary#",
-    "ontologies/inrae-vitioeno.owl",
-    Lang.RDFXML,
-    "vitioeno-vocabulary"
-    ));
-    return list;
+        List<OntologyFileDefinition> list = SPARQLExtension.super.getOntologiesFiles();
+        list.add(new OntologyFileDefinition(
+        "http://vitioeno.inrae.fr/vocabulary#",
+        "ontologies/inrae-vitioeno.owl",
+        Lang.RDFXML,
+        "vitioeno-vocabulary"
+        ));
+        return list;
     }
 
 }
